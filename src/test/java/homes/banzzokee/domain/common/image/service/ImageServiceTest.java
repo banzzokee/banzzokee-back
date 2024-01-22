55244e433b21b8cd60eedce88165ecc820fbd95e
nadoran78 <nadoran78@gmail.com>
2024-01-23 01:17:00 +0900

    refactor: S3 외부 서비스 배제하여 test코드 재작성(mock 적용)
    변경 사유 : 독립적인 단위 테스트 작성 (외부 서비스는 향후 통합 테스트에서 고려)

--- src/test/java/homes/banzzokee/domain/common/image/service/ImageServiceTest.java
+++ src/test/java/homes/banzzokee/domain/common/image/service/ImageServiceTest.java
@@ -1,69 +1,85 @@
 package homes.banzzokee.domain.common.image.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.amazonaws.services.s3.AmazonS3Client;
 import homes.banzzokee.domain.common.image.dto.ImageDto;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(MockitoExtension.class)
 class ImageServiceTest {
 
-  @Autowired
-  private TestRestTemplate restTemplate;
+  @Mock
+  private AmazonS3Client amazonS3Client;
 
-  @Autowired
+  @InjectMocks
   private ImageService imageService;
 
   @Test
-  @DisplayName("1개 파일 업로드 성공 테스트 및 파일 삭제 성공 테스트")
+  @DisplayName("1개 파일 업로드 성공 테스트")
   void successUploadFile() throws IOException {
     //given
     MultipartFile multipartFile = createMockMultipartFile();
 
+    given(amazonS3Client.getUrl(any(), anyString())).willReturn(
+        new URL("https://imageUrl.com"));
     //when
     ImageDto imageDto = imageService.uploadOneFile(multipartFile);
 
     // then
-    ResponseEntity<String> afterSaveResponse = restTemplate.getForEntity(imageDto.getUrl(),
-        String.class);
-    Assertions.assertEquals(afterSaveResponse.getBody(), "Upload Test");
+    assertEquals(imageDto.getUrl(), "https://imageUrl.com");
 
-    imageService.deleteFile(imageDto.getFilename());
-    ResponseEntity<String> afterDeleteResponse = restTemplate.getForEntity(imageDto.getUrl(),
-        String.class);
-    Assertions.assertEquals(afterDeleteResponse.getStatusCode(), HttpStatus.FORBIDDEN);
   }
 
   @Test
-  @DisplayName("다수의 파일 업로드 성공 테스트")
+  @DisplayName("여러 개 파일 업로드 성공 테스트")
   void successUploadManyFile() throws IOException {
-    // given
+    //given
     List<MultipartFile> multipartFiles = new ArrayList<>();
     for (int i = 0; i < 3; i++) {
       multipartFiles.add(createMockMultipartFile());
     }
-    // when
-    List<ImageDto> imageDtos = imageService.uploadManyFile(multipartFiles);
-    // then
-    for (ImageDto imageDto: imageDtos) {
-      ResponseEntity<String> response = restTemplate.getForEntity(imageDto.getUrl(),
-          String.class);
 
-      Assertions.assertEquals(response.getBody(), "Upload Test");
-      imageService.deleteFile(imageDto.getFilename());
-    }
+    given(amazonS3Client.getUrl(any(), anyString())).willReturn(
+        new URL("https://imageUrl.com"));
+    //when
+    List<ImageDto> imageDtoList = imageService.uploadManyFile(multipartFiles);
+    //then
+    assertEquals(3, imageDtoList.size());
+    assertEquals("https://imageUrl.com", imageDtoList.get(0).getUrl());
+    verify(amazonS3Client, times(3)).getUrl(any(), anyString());
+  }
+
+  @Test
+  @DisplayName("업로드 파일 삭제 성공 테스트")
+  void successDeleteFile() {
+    //given
+    String filename = "anything";
+    //when
+    imageService.deleteFile(filename);
+    //then
+    ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+    verify(amazonS3Client, times(1)).deleteObject(any(), captor.capture());
+    assertEquals(captor.getValue(), filename);
   }
 
   private MultipartFile createMockMultipartFile() throws IOException {
