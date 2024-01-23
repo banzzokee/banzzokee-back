0786840aac9a36d9f56bbe2fbc644d959791100e
thlee <e-tap@naver.com>
2024-01-23 21:33:05 +0900

    test: 사용자 팔로우, 언팔로우 테스트 작성

--- src/test/java/homes/banzzokee/global/util/MockMvcUtil.java
+++ src/test/java/homes/banzzokee/global/util/MockMvcUtil.java
@@ -2,6 +2,7 @@ package homes.banzzokee.global.util;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -11,6 +12,15 @@ import org.springframework.test.web.servlet.ResultActions;
 public class MockMvcUtil {
 
   private static final ObjectMapper objectMapper = new ObjectMapper();
+
+  public static ResultActions performPost(MockMvc mockMvc, String url, Object requestBody)
+      throws Exception {
+    return mockMvc.perform(
+            post(url)
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(requestBody)))
+        .andDo(print());
+  }
 
   public static ResultActions performGet(MockMvc mockMvc, String url) throws Exception {
     return mockMvc.perform(
