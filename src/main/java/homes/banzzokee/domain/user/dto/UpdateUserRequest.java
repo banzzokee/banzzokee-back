d8237f3a27d0f2c33dc12b619b06c3867a48fb94
thlee <e-tap@naver.com>
2024-01-25 08:43:30 +0900

    test: 사용자 프로필 테스트 작성
    @TestInstance PER_CLASS 경우
    verify 시 클래스 단위로 되기 때문에 정확한 테스트 확인 불가능하여 제거함

--- src/main/java/homes/banzzokee/domain/user/dto/UpdateUserRequest.java
+++ src/main/java/homes/banzzokee/domain/user/dto/UpdateUserRequest.java
@@ -1,6 +1,7 @@
 package homes.banzzokee.domain.user.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 
 /**
  * 사용자 프로필 수정 요청
@@ -8,6 +9,7 @@ import jakarta.validation.constraints.NotBlank;
  * @param nickname  닉네임
  * @param introduce 자기소개
  */
+@Builder
 public record UpdateUserRequest(
     @NotBlank String nickname,
     @NotBlank String introduce
---
+++ src/test/java/homes/banzzokee/global/util/MockDataUtil.java
@@ -0,0 +1,23 @@
+package homes.banzzokee.global.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.apache.tika.Tika;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+public class MockDataUtil {
+
+  private final static Tika tika = new Tika();
+
+  public static MultipartFile createMockMultipartFile(String file) throws IOException {
+    Path path = Path.of(file);
+    byte[] bytes = Files.readAllBytes(path);
+    String originFilename = StringUtils.getFilename(file);
+    String name = StringUtils.stripFilenameExtension(originFilename);
+    String contentType = tika.detect(path);
+    return new MockMultipartFile(name, originFilename, contentType, bytes);
+  }
+}
