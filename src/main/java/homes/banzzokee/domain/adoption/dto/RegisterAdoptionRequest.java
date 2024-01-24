b3c9d27e065be2169b991519b85ee3e994b4c9db
nadoran78 <nadoran78@gmail.com>
2024-01-24 20:55:58 +0900

    refactor: validation 처리 변경(not null) 및 shelterId 삭제

--- src/main/java/homes/banzzokee/domain/adoption/dto/RegisterAdoptionRequest.java
+++ src/main/java/homes/banzzokee/domain/adoption/dto/RegisterAdoptionRequest.java
@@ -5,20 +5,20 @@ import homes.banzzokee.global.validator.annotation.DateValid;
 import homes.banzzokee.global.validator.annotation.DogGenderValid;
 import homes.banzzokee.global.validator.annotation.DogSizeValid;
 import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record RegisterAdoptionDto (
-    @NotNull Long shelterId,
-    @NotNull @Size(max = 50) String title,
-    @NotNull @Size(max = 500) String content,
-    @BreedValid String breed,
-    @DogSizeValid String size,
-    boolean neutering,
-    @DogGenderValid String gender,
-    @Max(100) int age,
-    boolean healthChecked,
-    @DateValid String registeredAt
+public record RegisterAdoptionRequest(
+    @NotBlank @Size(max = 50) String title,
+    @NotBlank @Size(max = 500) String content,
+    @NotNull @BreedValid String breed,
+    @NotNull @DogSizeValid String size,
+    @NotNull boolean neutering,
+    @NotNull @DogGenderValid String gender,
+    @NotNull @Max(100) int age,
+    @NotNull boolean healthChecked,
+    @NotNull @DateValid String registeredAt
 
 ) {
 
