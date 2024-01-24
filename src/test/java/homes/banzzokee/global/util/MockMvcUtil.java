5a71f123c03d529ba76404089a1e970b5d29c5b1
thlee <e-tap@naver.com>
2024-01-24 21:10:14 +0900

    fix: 충돌 수정

--- src/test/java/homes/banzzokee/global/util/MockMvcUtil.java
+++ src/test/java/homes/banzzokee/global/util/MockMvcUtil.java
@@ -28,13 +28,4 @@ public class MockMvcUtil {
                 .contentType(APPLICATION_JSON))
         .andDo(print());
   }
-
-  public static ResultActions performPost(MockMvc mockMvc, String url, Object requestBody)
-      throws Exception {
-    return mockMvc.perform(
-            post(url)
-                .contentType(APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(requestBody)))
-        .andDo(print());
-  }
 }
