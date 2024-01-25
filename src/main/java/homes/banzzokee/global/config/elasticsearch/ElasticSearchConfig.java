f8af390e31a75b6559dca05944f8c89325847ade
nadoran78 <nadoran78@gmail.com>
2024-01-26 02:01:20 +0900

    chore: elastic search 설정(어노테이션 추가)

--- src/main/java/homes/banzzokee/global/config/elasticsearch/ElasticSearchConfig.java
+++ src/main/java/homes/banzzokee/global/config/elasticsearch/ElasticSearchConfig.java
@@ -1,10 +1,13 @@
 package homes.banzzokee.global.config.elasticsearch;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.lang.NonNull;
 
-
+@Configuration
+@EnableElasticsearchRepositories
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
   @NonNull
