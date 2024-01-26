ac77dda4273029c8e222595ac6bd1ed984347c76
nadoran78 <nadoran78@gmail.com>
2024-01-26 11:43:02 +0900

    fix: elasticsearch 연결을 위해 ssl, username, password 추가

--- src/main/java/homes/banzzokee/global/config/elasticsearch/ElasticSearchConfig.java
+++ src/main/java/homes/banzzokee/global/config/elasticsearch/ElasticSearchConfig.java
@@ -1,20 +1,43 @@
 package homes.banzzokee.global.config.elasticsearch;
 
+import co.elastic.clients.transport.TransportUtils;
+import javax.net.ssl.SSLContext;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.lang.NonNull;
 
+/**
+ * 스프링 부트 3.2.2와 호환되는 elasticsearch 및 kibana 버젼 : 8.11.3
+ */
 @Configuration
 @EnableElasticsearchRepositories
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
+  @Value("${elasticsearch.hostAndPort}")
+  private String hostAndPort;
+
+  @Value("${elasticsearch.fingerprint}")
+  private String fingerprint; //ssl 사용을 위해 elasticsearch 에서 추출
+
+  @Value("${elasticsearch.username}")
+  private String username;
+
+  @Value("${elasticsearch.password}")
+  private String password;
+
   @NonNull
   @Override
   public ClientConfiguration clientConfiguration() {
+
+    SSLContext sslContext = TransportUtils.sslContextFromCaFingerprint(fingerprint);
+
     return ClientConfiguration.builder()
-        .connectedTo("localhost:9200")
+        .connectedTo(hostAndPort)
+        .usingSsl(sslContext)
+        .withBasicAuth(username, password)
         .build();
   }
 }
