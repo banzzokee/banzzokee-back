c8ab021bcdbd0d158cb3c0fee72636d2ec3277f1
eTaphee <e-tap@naver.com>
2024-01-27 17:41:46 +0900

    test: 테스트 컨테이너 비활성

--- src/test/java/homes/banzzokee/infra/fileupload/service/LocalStackTestContainersTest.java
+++ src/test/java/homes/banzzokee/infra/fileupload/service/LocalStackTestContainersTest.java
@@ -7,6 +7,7 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -14,16 +15,19 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.org.apache.commons.io.IOUtils;
 import org.testcontainers.utility.DockerImageName;
 
+@Disabled
 @Testcontainers
 public class LocalStackTestContainersTest {
-  private static final DockerImageName LOCALSTACK_IMAGE = DockerImageName.parse("localstack/localstack");
+
+  private static final DockerImageName LOCALSTACK_IMAGE = DockerImageName.parse(
+      "localstack/localstack");
 
   @Container
   LocalStackContainer localStackContainer = new LocalStackContainer(LOCALSTACK_IMAGE)
       .withServices(S3);
 
   @Test
-  void test(){
+  void test() {
     AmazonS3 amazonS3 = AmazonS3ClientBuilder
         .standard()
         .withEndpointConfiguration(localStackContainer.getEndpointConfiguration(S3))
@@ -32,14 +36,15 @@ public class LocalStackTestContainersTest {
 
     String bucketName = "banzzokee";
     amazonS3.createBucket(bucketName);
-    System.out.println(bucketName +" 버킷 생성");
+    System.out.println(bucketName + " 버킷 생성");
 
     String key = "filename";
     String content = "fileContent";
     amazonS3.putObject(bucketName, key, content);
-    System.out.println("파일을 업로드하였습니다. key=" + key +", content=" + content);
+    System.out.println("파일을 업로드하였습니다. key=" + key + ", content=" + content);
 
-    List<String> results = IOUtils.readLines(amazonS3.getObject(bucketName, key).getObjectContent(), "utf-8");
+    List<String> results = IOUtils.readLines(
+        amazonS3.getObject(bucketName, key).getObjectContent(), "utf-8");
     System.out.println("파일을 가져왔습니다. = " + results);
 
     Assertions.assertEquals(1, results.size());
