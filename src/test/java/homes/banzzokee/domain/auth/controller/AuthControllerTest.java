b83c77d3d090d4264976d46c3cb185db98424ba5
Woogie95 <choisw950928@gmail.com>
2024-01-29 22:11:10 +0900

    chore: git 충돌 해결 커밋

--- src/test/java/homes/banzzokee/domain/auth/controller/AuthControllerTest.java
+++ src/test/java/homes/banzzokee/domain/auth/controller/AuthControllerTest.java
@@ -57,8 +57,11 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @ExtendWith(SpringExtension.class)
