efa16069a4cd54fc97f63b64148512b684d86897
Woogie95 <choisw950928@gmail.com>
2024-01-29 23:52:45 +0900

    chore: git 충돌 해결 커밋

--- src/main/java/homes/banzzokee/domain/auth/dto/EmailDto.java
+++ src/main/java/homes/banzzokee/domain/auth/dto/EmailDto.java
@@ -2,10 +2,6 @@ package homes.banzzokee.domain.auth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
@@ -15,9 +11,10 @@ public class EmailDto {
   /**
    * email 인증할 이메일
    */
+public record EmailDto(
 
-  @NotBlank(message = "email 은 필수 입력 항목입니다.")
-  @Email(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "올바른 이메일 형식을 입력해주세요.")
-  private String email;
+    @NotBlank(message = "email 은 필수 입력 항목입니다.")
+    @Email(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$", message = "올바른 이메일 형식을 입력해주세요.")
+    String email) {
 
 }
