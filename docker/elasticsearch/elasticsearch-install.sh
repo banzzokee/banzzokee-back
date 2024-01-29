a429b6525126c5dfe5d266b11e571385023b0154
nadoran78 <nadoran78@gmail.com>
2024-01-29 17:45:15 +0900

    refactor: kibana 작동을 위해 ssl 설정 추가

--- docker/elasticsearch/elasticsearch-install.sh
+++ docker/elasticsearch/elasticsearch-install.sh
@@ -2,7 +2,7 @@
 
 # elasticsearch 컨테이너 이미지 pull 및 elasticsearch 실행
 docker pull docker.elastic.co/elasticsearch/elasticsearch:8.11.3
-docker run -d --name elasticsearch-banzzokee -p 9200:9200 -p 9300:9300 --env-file ./es.env docker.elastic.co/elasticsearch/elasticsearch:8.11.3
+docker run -d --name elasticsearch-banzzokee -p 9200:9200 -p 9300:9300 -it -m 1GB -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:8.11.3
 
 # elasticsearch 내 nori분석기 설치
 docker exec -it elasticsearch-banzzokee bin/elasticsearch-plugin install analysis-nori
@@ -12,3 +12,15 @@ docker restart elasticsearch-banzzokee
 
 # username, password 설정(username : banzzokee, password : funnycode)
 docker exec -it --user root elasticsearch-banzzokee /usr/share/elasticsearch/bin/elasticsearch-users useradd banzzokee -p funnycode -r superuser
+
+# kibana 설치
+docker pull docker.elastic.co/kibana/kibana:8.11.3
+docker run -d --name kibana-banzzokee --link elasticsearch-banzzokee:elasticsearch-banzzokee -p 5601:5601 docker.elastic.co/kibana/kibana:8.11.3
+
+# fingerprint 확인
+echo "wait for 40seconds"
+sleep 40
+docker logs elasticsearch-banzzokee
+
+# 키바나 인증코드 확인
+docker exec -it kibana-banzzokee bin/kibana-verification-code
