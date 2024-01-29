950bfbf6a66146288822f9839fc78f8c0b462377
Woogie95 <choisw950928@gmail.com>
2024-01-29 18:46:15 +0900

    feat: 자사 로그인 서비스 API

--- src/main/java/homes/banzzokee/domain/auth/service/AuthService.java
+++ src/main/java/homes/banzzokee/domain/auth/service/AuthService.java
@@ -1,11 +1,12 @@
 package homes.banzzokee.domain.auth.service;
 
-import homes.banzzokee.domain.auth.dto.EmailDto;
-import homes.banzzokee.domain.auth.dto.EmailVerifyDto;
-import homes.banzzokee.domain.auth.dto.SignupDto;
+import homes.banzzokee.domain.auth.dto.*;
 import homes.banzzokee.domain.auth.exception.*;
+import homes.banzzokee.domain.type.LoginType;
+import homes.banzzokee.domain.type.Role;
 import homes.banzzokee.domain.user.dao.UserRepository;
 import homes.banzzokee.domain.user.entity.User;
+import homes.banzzokee.global.security.jwt.JwtTokenProvider;
 import homes.banzzokee.global.util.redis.RedisService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +17,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.Duration;
-import java.util.Random;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 @Service
@@ -27,49 +29,55 @@ public class AuthService {
   private static final String EMAIL_TEXT = "인증 코드는 %s 입니다.";
   private static final int VERIFICATION_CODE_EXPIRATION_TIME = 3;
   private static final int VERIFICATION_CODE_MIN_VALUE = 100000;
-  private static final int VERIFICATION_CODE_MAX_VALUE = 900000;
+  private static final int VERIFICATION_CODE_MAX_VALUE = 1000000;
 
   private final RedisService redisService;
 
   private final JavaMailSender mailSender;
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
+  private final JwtTokenProvider jwtTokenProvider;
 
   @Transactional
-  public void signup(SignupDto signupDto) {
-    if (userRepository.existsByEmail(signupDto.getEmail())) {
+  public void signup(SignupRequest signupRequest) {
+    if (userRepository.existsByEmailAndDeletedAtIsNull(signupRequest.getEmail())) {
       throw new EmailDuplicatedException();
     }
-    String confirmPassword = signupDto.getConfirmPassword();
-    if (!signupDto.getPassword().equals(confirmPassword)) {
+    String confirmPassword = signupRequest.getConfirmPassword();
+    if (!signupRequest.getPassword().equals(confirmPassword)) {
       throw new ConfirmPasswordUnMatchException();
     }
-    if (userRepository.existsByNickname(signupDto.getNickname())) {
+    if (userRepository.existsByNicknameAndDeletedAtIsNull(signupRequest.getNickname())) {
       throw new NicknameDuplicatedException();
     }
-    User user = signupDto.toEntity();
-    user.setPassword(passwordEncoder.encode(signupDto.getPassword()));
-    userRepository.save(user);
+    userRepository.save(User.builder()
+        .email(signupRequest.getEmail())
+        .password(passwordEncoder.encode(signupRequest.getPassword()))
+        .nickname(signupRequest.getNickname())
+        .role(Set.of(Role.USER))
+        .loginType(LoginType.EMAIL)
+        .build());
   }
 
-  public void sendVerificationCode(EmailDto emailDto) {
+  public void sendVerificationCode(EmailRequest emailRequest) {
     String code = generateVerificationCode();
-    redisService.setData(emailDto.email(), code, Duration.ofMinutes(VERIFICATION_CODE_EXPIRATION_TIME).getSeconds());
+    redisService.setData(emailRequest.getEmail(), code, Duration.ofMinutes(VERIFICATION_CODE_EXPIRATION_TIME).getSeconds());
     SimpleMailMessage message = new SimpleMailMessage();
-    message.setTo(emailDto.email());
+    message.setTo(emailRequest.getEmail());
     message.setSubject(EMAIL_SUBJECT);
     message.setText(String.format(EMAIL_TEXT, code));
     mailSender.send(message);
   }
 
   private String generateVerificationCode() {
-    return String.valueOf(VERIFICATION_CODE_MIN_VALUE + new Random().nextInt(VERIFICATION_CODE_MAX_VALUE));
+    return String.valueOf(ThreadLocalRandom.current()
+        .nextInt(VERIFICATION_CODE_MIN_VALUE, VERIFICATION_CODE_MAX_VALUE));
   }
 
-  public void verifyEmail(EmailVerifyDto emailVerifyDto) {
-    String email = emailVerifyDto.getEmail();
-    String code = emailVerifyDto.getCode();
-    String redisCode = (String) redisService.getData(email);
+  public void verifyEmail(EmailVerifyRequest emailVerifyRequest) {
+    String email = emailVerifyRequest.getEmail();
+    String code = emailVerifyRequest.getCode();
+    String redisCode = redisService.getData(email);
     if (redisCode == null) {
       throw new EmailCodeUnmatchedException();
     }
@@ -80,6 +88,23 @@ public class AuthService {
   }
 
   public boolean checkNickname(String nickname) {
-    return !userRepository.existsByNickname(nickname);
+    return !userRepository.existsByNicknameAndDeletedAtIsNull(nickname);
+  }
+
+  public TokenResponse signIn(SignInRequest signInRequest) {
+    String email = signInRequest.getEmail();
+    String password = signInRequest.getPassword();
+    User user = userRepository.findByEmail(email)
+        .orElseThrow(EmailNotFoundException::new);
+    if (!passwordEncoder.matches(password, user.getPassword())) {
+      throw new PasswordUnmatchedException();
+    }
+    String accessToken = jwtTokenProvider.createAccessToken(user);
+    String refreshToken = jwtTokenProvider.createRefreshToken(user);
+    redisService.setRefreshToken(user.getEmail(), refreshToken, jwtTokenProvider.getRefreshTokenExpire());
+    return TokenResponse.builder()
+        .accessToken(accessToken)
+        .refreshToken(refreshToken)
+        .build();
   }
 }
