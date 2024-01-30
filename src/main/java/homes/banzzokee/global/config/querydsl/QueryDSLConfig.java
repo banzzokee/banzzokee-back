39bb00256b2f291b0371a09a6b513dc941fa8841
Taejun Park <ekswn120@gmail.com>
2024-01-30 20:38:52 +0900

    chore: queryDSL configuration

---
+++ src/main/java/homes/banzzokee/global/config/querydsl/QueryDSLConfig.java
@@ -0,0 +1,22 @@
+package homes.banzzokee.global.config.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * QueryDsl 설정
+ */
+@Configuration
+public class QueryDSLConfig {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Bean
+  public JPAQueryFactory jpaQueryFactory(){
+    return new JPAQueryFactory(entityManager);
+  }
+}
