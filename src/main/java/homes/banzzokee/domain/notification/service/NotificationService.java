1043caac6f9cd603813adc8f913b8682a343d6e9
eTaphee <e-tap@naver.com>
2024-01-30 14:51:13 +0900

    feature: 토큰 등록 사용자 비교 로직 추가

--- src/main/java/homes/banzzokee/domain/notification/service/NotificationService.java
+++ src/main/java/homes/banzzokee/domain/notification/service/NotificationService.java
@@ -6,6 +6,8 @@ import homes.banzzokee.domain.notification.entity.FcmToken;
 import homes.banzzokee.domain.user.dao.UserRepository;
 import homes.banzzokee.domain.user.entity.User;
 import homes.banzzokee.domain.user.exception.UserNotFoundException;
+import homes.banzzokee.global.error.exception.NoAuthorizedException;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +19,13 @@ public class NotificationService {
   private final FcmTokenRepository fcmTokenRepository;
   private final UserRepository userRepository;
 
+  /**
+   * FCM 토큰 등록
+   *
+   * @param request   토큰 등록 요청
+   * @param userAgent User-Agent
+   * @param userId    토큰을 등록할 사용자 아이디
+   */
   @Transactional
   public void registerFcmToken(FcmTokenRegisterRequest request, String userAgent,
       long userId) {
@@ -24,7 +33,10 @@ public class NotificationService {
 
     fcmTokenRepository.findByToken(request.getToken())
         .ifPresentOrElse(
-            FcmToken::refresh,
+            (token) -> {
+              throwIfUserIsNotTokenUser(user, token);
+              token.refresh();
+            },
             () -> fcmTokenRepository.save(FcmToken.builder()
                 .token(request.getToken())
                 .userAgent(userAgent)
@@ -32,7 +44,25 @@ public class NotificationService {
                 .build()));
   }
 
+  /**
+   * 사용자를 반환한다.
+   *
+   * @param userId 사용자 아이디
+   * @return 사용자
+   */
   private User findUserByIdOrThrow(long userId) {
     return userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+  }
+
+  /**
+   * 토큰을 등록한 사용자가 아니라면 예외를 발생한다.
+   *
+   * @param user  확인할 사용자
+   * @param token 토큰
+   */
+  private void throwIfUserIsNotTokenUser(User user, FcmToken token) {
+    if (!Objects.equals(user.getId(), token.getUser().getId())) {
+      throw new NoAuthorizedException();
+    }
   }
 }
