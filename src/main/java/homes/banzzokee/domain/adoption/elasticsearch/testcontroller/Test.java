818d6cee6a3f8f4ec4312ba5bf737b2ed38edc2c
nadoran78 <nadoran78@gmail.com>
2024-01-30 22:20:34 +0900

    test: ES에 list 입력되는지 여부 확인 목적으로 수정

--- src/main/java/homes/banzzokee/domain/adoption/elasticsearch/testcontroller/Test.java
+++ src/main/java/homes/banzzokee/domain/adoption/elasticsearch/testcontroller/Test.java
@@ -2,10 +2,11 @@ package homes.banzzokee.domain.adoption.elasticsearch.testcontroller;
 
 import homes.banzzokee.domain.adoption.elasticsearch.dao.AdoptionSearchRepository;
 import homes.banzzokee.domain.adoption.elasticsearch.document.AdoptionDocument;
+import homes.banzzokee.domain.type.S3Object;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +17,17 @@ public class Test {
 
   private final AdoptionSearchRepository adoptionSearchRepository;
 
+  @DeleteMapping
+  public void test2() {
+    adoptionSearchRepository.deleteAll();
+  }
+
   @GetMapping
   public String test() {
     AdoptionDocument adoptionDocument = AdoptionDocument.builder()
         .id(1L)
         .breed("견종")
+        .images(List.of(new S3Object("1"), new S3Object("2")))
         .build();
     adoptionSearchRepository.save(adoptionDocument);
     String string = adoptionSearchRepository.findByBreed("견종").get(0).getBreed();
