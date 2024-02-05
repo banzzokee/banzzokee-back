e016ea8bc8c7bcef2ed0ff30daa52a82f03d7380
Taejun Park <ekswn120@gmail.com>
2024-02-05 18:54:54 +0900

    refactor: 코드 정리

--- src/main/java/homes/banzzokee/global/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/homes/banzzokee/global/security/jwt/JwtAuthenticationFilter.java
@@ -40,7 +40,6 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
                                   FilterChain filterChain) throws ServletException, IOException {
     String token = resolveToken(request);
-    log.info("[filter] token : {}", token);
     if (token != null) {
       try {
         getAuthenticate(token);
@@ -72,9 +71,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     jwtTokenProvider.validateToken(token);
     String userEmail = jwtTokenProvider.getUserEmailFromToken(token);
     UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
-//    log.info("[getAuthenticate] Auth : {}", userDetails.getAuthorities().);
     Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     SecurityContextHolder.getContext().setAuthentication(authentication);
-//    authentication.
   }
 }
