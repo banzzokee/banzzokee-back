5dd07d519055ba0d3b9cceadaa7550974c346b10
Taejun Park <ekswn120@gmail.com>
2024-02-19 22:54:42 +0900

    refactor: add customAuthenticationEntryPoint

--- src/main/java/homes/banzzokee/global/error/CustomAuthenticationEntryPoint.java
+++ src/main/java/homes/banzzokee/global/error/CustomAuthenticationEntryPoint.java
@@ -9,48 +9,50 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import homes.banzzokee.global.security.exception.RefreshTokenExpiredException;
-import homes.banzzokee.global.security.exception.TokenInvalidException;
-import io.jsonwebtoken.ExpiredJwtException;
-import jakarta.servlet.FilterChain;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import org.springframework.web.filter.OncePerRequestFilter;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class ExceptionHandlerFilter extends OncePerRequestFilter {
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
   private final ObjectMapper objectMapper;
 
   @Override
-  protected void doFilterInternal(HttpServletRequest request,
-      HttpServletResponse response, FilterChain filterChain)
-      throws IOException {
-    try {
-      filterChain.doFilter(request, response);
-    } catch (AccessDeniedException e) {
-      setErrorResponse(NO_AUTHORIZED, request, response, e);
-    } catch (ExpiredJwtException e) {
-      setErrorResponse(ACCESS_TOKEN_EXPIRED, request, response, e);
-    } catch (TokenInvalidException e) {
-      setErrorResponse(INVALID_TOKEN, request, response, e);
-    } catch (RefreshTokenExpiredException e) {
-      setErrorResponse(REFRESH_TOKEN_EXPIRED, request, response, e);
-    } catch (Exception e) {
-      setErrorResponse(INTERNAL_ERROR, request, response, e);
+  public void commence(HttpServletRequest request, HttpServletResponse response,
+      AuthenticationException authException) throws IOException {
+
+    // Filter 로 부터 넘어온 request 에서 getAttribute()로 발생한 예외 추출
+    Object exception = request.getAttribute("exception");
+
+    if (exception == NO_AUTHORIZED) {
+      setErrorResponse(NO_AUTHORIZED, request, response);
+
+    } else if (exception == ACCESS_TOKEN_EXPIRED) {
+      setErrorResponse(ACCESS_TOKEN_EXPIRED, request, response);
+
+    } else if (exception == INVALID_TOKEN) {
+      setErrorResponse(INVALID_TOKEN, request, response);
+
+    } else if (exception == REFRESH_TOKEN_EXPIRED) {
+      setErrorResponse(REFRESH_TOKEN_EXPIRED, request, response);
+
+    } else if (exception == INTERNAL_ERROR) {
+      setErrorResponse(INTERNAL_ERROR, request, response);
     }
+
   }
 
   private void setErrorResponse(ErrorCode errorCode, HttpServletRequest request,
-      HttpServletResponse response, Exception e) throws IOException {
-    log.error("{} is occurred. uri:{}", e.getClass().getName(),
+      HttpServletResponse response) throws IOException {
+    log.error("{} is occurred. uri:{}", errorCode.name(),
         request.getRequestURI());
 
     response.setContentType(APPLICATION_JSON_VALUE);
--- src/main/java/homes/banzzokee/global/security/config/SecurityConfig.java
+++ src/main/java/homes/banzzokee/global/security/config/SecurityConfig.java
@@ -7,7 +7,7 @@ import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.http.HttpMethod.PUT;
 
 import homes.banzzokee.global.error.AccessDeniedHandlerImpl;
-import homes.banzzokee.global.error.ExceptionHandlerFilter;
+import homes.banzzokee.global.error.CustomAuthenticationEntryPoint;
 import homes.banzzokee.global.security.jwt.JwtAuthenticationFilter;
 import homes.banzzokee.global.security.oauth2.handler.OAuth2FailureHandler;
 import homes.banzzokee.global.security.oauth2.handler.OAuth2SuccessHandler;
@@ -28,10 +28,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
-  private final ExceptionHandlerFilter exceptionHandlerFilter;
   private final OAuth2SuccessHandler oAuth2SuccessHandler;
   private final OAuth2FailureHandler oAuth2FailureHandler;
   private final OAuth2UserDetailsServiceImpl oAuth2UserDetailsService;
+  private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -50,15 +50,19 @@ public class SecurityConfig {
             .requestMatchers("/api/shelters/{shelterId}/verify").hasAnyRole("ADMIN")
             .requestMatchers(POST, "/api/shelters").hasAnyRole("USER", "SHELTER")
             .requestMatchers("/api/shelters/**").hasAnyRole("SHELTER")
-            .requestMatchers(GET, "/api/adoptions", "/api/adoptions/{adoptionId}").permitAll()
+            .requestMatchers(GET, "/api/adoptions", "/api/adoptions/{adoptionId}")
+            .permitAll()
             .requestMatchers(POST, "/api/adoptions").hasAnyRole("SHELTER")
             .requestMatchers(PUT, "api/adoptions/{adoptionId}").hasAnyRole("SHELTER")
-            .requestMatchers(PATCH, "/api/adoptions/{adoptionId}/status").hasAnyRole("SHELTER")
-            .requestMatchers(DELETE, "/api/adoptions/{adoptionID}").hasAnyRole("USER", "ADMIN")
+            .requestMatchers(PATCH, "/api/adoptions/{adoptionId}/status")
+            .hasAnyRole("SHELTER")
+            .requestMatchers(DELETE, "/api/adoptions/{adoptionID}")
+            .hasAnyRole("USER", "ADMIN")
             .requestMatchers(GET, "/api/reviews", "/api/reviews/{reviewId}").permitAll()
             .requestMatchers(POST, "api/reviews").hasAnyRole("USER")
             .requestMatchers(PUT, "/api/reviews/{reviewId}").hasAnyRole("USER")
-            .requestMatchers(DELETE, "/api/reviews/{reviewId}").hasAnyRole("USER", "ADMIN")
+            .requestMatchers(DELETE, "/api/reviews/{reviewId}")
+            .hasAnyRole("USER", "ADMIN")
             .requestMatchers("/api/bookmarks/**").hasAnyRole("USER")
             .requestMatchers("/api/notifications/**")
             .hasAnyRole("USER", "ADMIN", "SHELTER")
@@ -66,16 +70,22 @@ public class SecurityConfig {
             .hasAnyRole("USER", "ADMIN", "SHELTER")
             .requestMatchers("/ws-stomp/**").permitAll()
             .anyRequest().authenticated())
+
         .oauth2Login(oauth2 -> oauth2
             .loginPage("/oauth2/authorization/google")
             .successHandler(oAuth2SuccessHandler)
             .failureHandler(oAuth2FailureHandler)
             .userInfoEndpoint(userInfo -> userInfo
                 .userService(oAuth2UserDetailsService)))
+
+        .exceptionHandling(ex -> {
+          ex.accessDeniedHandler(new AccessDeniedHandlerImpl());
+          ex.authenticationEntryPoint(customAuthenticationEntryPoint);
+        })
+
         .addFilterBefore(jwtAuthenticationFilter,
-            UsernamePasswordAuthenticationFilter.class)
-        .addFilterBefore(exceptionHandlerFilter, JwtAuthenticationFilter.class)
-        .exceptionHandling(ex -> ex.accessDeniedHandler(new AccessDeniedHandlerImpl()));
+            UsernamePasswordAuthenticationFilter.class);
+
     return httpSecurity.build();
   }
 }
