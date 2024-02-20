9698c8231d24d8dbcd1286c217e89020efd089df
eTaphee <e-tap@naver.com>
2024-02-20 15:14:49 +0900

    test: 알림 목록 조회, 읽음 테스트 작성

--- src/main/java/homes/banzzokee/domain/notification/dto/NotificationDto.java
+++ src/main/java/homes/banzzokee/domain/notification/dto/NotificationDto.java
@@ -1,10 +1,12 @@
 package homes.banzzokee.domain.notification.dto;
 
 import java.time.LocalDateTime;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@Builder
 @RequiredArgsConstructor
 public class NotificationDto {
 
